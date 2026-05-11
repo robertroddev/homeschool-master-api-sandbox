51bf381f9e3adabc9eb852faaa283be0979c823b
Robert Masters <robertmasters2478@gmail.com>
2026-05-11 16:02:03 -0400

    allow for camel case params from front end.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  before_action :deep_snake_case_params
+
+  private
+
+  def deep_snake_case_params
+    request.parameters.deep_transform_keys!(&:underscore)
+  end
 end
